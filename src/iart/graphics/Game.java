3853e8af89012ea0c0d902ce3c5eee6f0ef93285
IneSantos <inesscarneiro94@gmail.com>
2016-05-20 17:13:50 +0100

    almost done for A*

--- src/iart/graphics/Game.java
+++ src/iart/graphics/Game.java
@@ -1,8 +1,10 @@
 package iart.graphics;
 
 import iart.game.Hopeless;
+import iart.utilities.*;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -10,19 +12,21 @@ public class Game extends JFrame {
 
     private static int PREF_W = 1000;
     private static int PREF_H = 600;
+    public static int score = 0;
+    public static ArrayList<iart.utilities.Point> bestMoves;
+    public static Hopeless hopeAStar3 = new Hopeless(10,20,4);
+
+    static CenterPanel centerPanel = new CenterPanel();
 
     public Game(){
         super("Hopeless");
 
-        Hopeless hopeAStar3 = new Hopeless(10,20,4);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        CenterPanel centerPanel = new CenterPanel(hopeAStar3);
         setLayout(new BorderLayout());
         NorthPanel north = new NorthPanel();
         Panel east = new Panel();
         east.add(Box.createHorizontalStrut(100));
-        WestPanel west = new WestPanel(9999);
+        WestPanel west = new WestPanel();
         SouthPanel south = new SouthPanel();
 
         PREF_W = west.PREF_W + centerPanel.PREF_W + 100 ;
--- src/iart/graphics/WestPanel.java
+++ src/iart/graphics/WestPanel.java
@@ -1,7 +1,17 @@
 package iart.graphics;
 
+import iart.algorithms.AStarSearch;
+import iart.game.Hopeless;
+import iart.utilities.*;
+import sun.nio.cs.ext.MacCentralEurope;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by inesa on 19/05/3016.
@@ -14,12 +24,13 @@ public class WestPanel extends JPanel {
     private JButton greedy;
     private JButton aStar;
     private JButton idaStar;
+    Hopeless hopeAStar3;
 
     public static final int PREF_W = 200;
     public static final int PREF_H = 300;
 
 
-    public WestPanel(int score) {
+    public WestPanel() {
         add(Box.createHorizontalStrut(100));
         //setBorder(BorderFactory.createLineBorder(Color.black));
 
@@ -31,29 +42,87 @@ public class WestPanel extends JPanel {
         idaStar = new JButton("IDASTAR");
 
         add(dfs);
-        dfs.setPreferredSize(new Dimension(100,30));
+        dfs.setPreferredSize(new Dimension(100, 30));
         add(bfs);
-        bfs.setPreferredSize(new Dimension(100,30));
+        bfs.setPreferredSize(new Dimension(100, 30));
         add(iddfs);
-        iddfs.setPreferredSize(new Dimension(100,30));
+        iddfs.setPreferredSize(new Dimension(100, 30));
         add(aStar);
-        aStar.setPreferredSize(new Dimension(100,30));
+        aStar.setPreferredSize(new Dimension(100, 30));
         add(idaStar);
-        idaStar.setPreferredSize(new Dimension(100,30));
+        idaStar.setPreferredSize(new Dimension(100, 30));
         add(greedy);
-        greedy.setPreferredSize(new Dimension(100,30));
+        greedy.setPreferredSize(new Dimension(100, 30));
 
 
-        JLabel jlabel = new JLabel("Score: " + score);
-        jlabel.setFont(new Font("Verdana",1,15));
+        JLabel jlabel = new JLabel("Score: " + Game.score);
+        jlabel.setFont(new Font("Verdana", 1, 15));
         jlabel.setHorizontalAlignment(0);
         jlabel.setVerticalAlignment(0);
-        jlabel.setPreferredSize(new Dimension(200,60));
+        jlabel.setPreferredSize(new Dimension(200, 60));
         add(jlabel);
+
+        buttons();
     }
 
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(PREF_W, PREF_H);
+    }
+
+    public void buttons() {
+
+        aStar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ArrayList<Integer> initialTable = new ArrayList<>(Game.hopeAStar3.table);
+                AStarSearch rip = new AStarSearch(Game.hopeAStar3, false);
+                Thread t = new Thread(rip);
+                t.start();
+
+                try {
+                    Thread thread = new Thread() {
+                        public void run() {
+                                    // wait until we have data to complete a readLine()
+                                    try {
+                                            Thread.sleep(200);
+                                        } catch (InterruptedException e1) {
+                                        e1.printStackTrace();
+                                    }
+                                    rip.setAccelarator(true);
+                                    }
+                                };
+
+                    thread.start();
+                    t.join();
+                    thread.interrupt();
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+
+                ArrayList<iart.utilities.Point> bestMoves = rip.getAStarMoves();
+
+                Game.hopeAStar3.table = initialTable;
+
+                int points = 0;
+
+                if(rip.getBestScore()!= 0)
+                    for(iart.utilities.Point move : bestMoves)
+                    {
+                        points += Game.hopeAStar3.makePlay(move,new ArrayList<iart.utilities.Point>());
+                        Game.centerPanel.updateUI();
+                    }
+                System.out.println("Final ----");
+                Game.hopeAStar3.print();
+
+
+                System.out.println("Moves = " + bestMoves);
+                System.out.println("A* Score = " + rip.getBestScore());
+
+                System.out.println("A* Sum Of Points = " + points);
+
+
+
+            }
+        });
     }
 }
